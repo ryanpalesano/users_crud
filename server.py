0b128ae004a0ef474fb929da28c5a9cafc65110b
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-03-20 16:49:37 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,82 @@
+from user import User
+from flask import Flask, redirect, request, render_template
+
+app = Flask(__name__)
+
+# View ALL USERS
+
+@app.route('/users')
+def index():
+    return redirect('/')
+
+
+@app.route('/')
+def users():
+    context = {
+        'users': User.get_all()
+    }
+    return render_template("read_all.html", **context)
+
+# Create NEW USER
+
+
+@app.route('/users/new')
+def new():
+    return render_template("create.html")
+
+
+@app.route('/users/create', methods=['POST'])
+def create():
+    User.save(request.form)
+    return redirect('/users')
+
+# EDIT USER
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id": id
+    }
+    context = {
+        'user': User.get_one(data)
+    }
+    return render_template("edit.html", **context)
+
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+# SHOW one user
+
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data = {
+        "id": id
+    }
+    context = {
+        'user': User.get_one(data)
+    }
+    return render_template("read_one.html", **context)
+
+# DELETE one user
+
+
+@app.route('/user/delete/<int:user_id>')
+def delete(user_id):
+    data = {
+        'id': user_id
+    }
+    User.delete(data)
+    return redirect('/users')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# left one goes in the html
+
+# right one goes in from the data 
---
+++ user.py
@@ -0,0 +1,57 @@
+from mysqlconnection import connectToMySQL
+
+db = "users"
+
+
+class User:
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email_addy']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    def full_name(self):
+        fullname = f"{self.first_name} {self.last_name}"
+        return fullname
+
+    @classmethod
+    def save(cls, data):
+        query = """
+        INSERT INTO users ( first_name, last_name, email_addy, created_at, updated_at )
+        VALUES ( %(fname)s, %(lname)s, %(email_addy)s, NOW(), NOW() );"""
+        return connectToMySQL(db).query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        query = """
+        UPDATE users
+        SET first_name = %(fname)s, last_name = %(lname)s, email_addy = %(email)s, updated_at = NOW()
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = """
+        DELETE FROM users
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query, data)
+
+    @classmethod
+    def get_one(cls, data):
+        query = """
+        SELECT * FROM users
+        WHERE id = %(id)s;"""
+        result = connectToMySQL(db).query_db(query, data)
+        return cls(result[0])
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(db).query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
