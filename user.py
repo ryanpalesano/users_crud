543583909906b8c3bf1d210bc33002a381574143
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-07-17 16:24:35 -0500

    Update user.py

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ class User:
 
     def __init__(self, data):
         self.id = data['id']
-        self.first_name = data['name']
+        self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email_addy']
         self.created_at = data['created_at']
